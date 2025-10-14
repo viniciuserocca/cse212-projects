60b43207b109e817ffdd9b237fe1aba108e679f7
Vinicius Rocca <vinicius.erocca@gmail.com>
2025-10-14 02:33:09 -0300

    week 06 activity - code: trees

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -12,6 +12,9 @@ public class Node
     public void Insert(int value)
     {
         // TODO Start Problem 1
+        if (value == Data) {
+            return;
+        }
 
         if (value < Data)
         {
@@ -34,12 +37,31 @@ public class Node
     public bool Contains(int value)
     {
         // TODO Start Problem 2
-        return false;
+         if (value == Data) {
+            return true;
+        }
+
+        if (value < Data) {
+            if (Left is null) {
+                return false;
+            }
+            
+            return Left.Contains(value);
+        } else {
+            if (Right is null)
+            {
+                return false;
+            }
+            
+            return Right.Contains(value);
+        }
     }
 
     public int GetHeight()
     {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int leftHeight = (Left is null) ? 0 : Left.GetHeight();
+        int rightHeight = (Right is null) ? 0 : Right.GetHeight();
+        return 1 + Math.Max(leftHeight, rightHeight);
     }
 }
